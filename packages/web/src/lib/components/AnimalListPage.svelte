baef38ec798b986f0a6a78338078a9f6087dc7dc
Wooseop Kim <wooseop@protonmail.com>
2024-12-27 22:14:03 +0900

    Update array value in a null-safe way

--- packages/web/src/lib/components/AnimalListPage.svelte
+++ packages/web/src/lib/components/AnimalListPage.svelte
@@ -1,17 +1,17 @@
 <script lang="ts">
+	import type { AnimalRecord } from '$lib/db/AnimalRecord';
+	import type { AnimalPage } from '$lib/store/animalPages';
 	import type { PostgrestError } from '@supabase/supabase-js';
 	import Error from './AnimalListPage/Error.svelte';
-	import Loading from './AnimalListPage/Loading.svelte';
 	import List from './AnimalListPage/List.svelte';
-	import type { AnimalPage } from '$lib/store/animalPages';
-	import type { AnimalRecord } from '$lib/db/AnimalRecord';
+	import Loading from './AnimalListPage/Loading.svelte';
 
 	export let data: AnimalPage;
 	export let error: PostgrestError | null = null;
 	let content: AnimalRecord[];
 
 	$: if (!data.loading && !error) {
-		content = data.content!;
+		content = data.content ?? [];
 	}
 </script>
 
