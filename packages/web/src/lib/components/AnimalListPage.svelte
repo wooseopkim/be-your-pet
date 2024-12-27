2dbd3c9df3d35a896315a84c15c3839f62d1b3e5
Wooseop Kim <wooseop@protonmail.com>
2024-12-27 22:34:59 +0900

    Import orders

--- packages/web/src/lib/components/AnimalListPage.svelte
+++ packages/web/src/lib/components/AnimalListPage.svelte
@@ -1,10 +1,10 @@
 <script lang="ts">
-	import type { AnimalRecord } from '$lib/db/AnimalRecord';
-	import type { AnimalPage } from '$lib/store/animalPages';
 	import type { PostgrestError } from '@supabase/supabase-js';
 	import Error from './AnimalListPage/Error.svelte';
 	import List from './AnimalListPage/List.svelte';
 	import Loading from './AnimalListPage/Loading.svelte';
+	import type { AnimalPage } from '$lib/store/animalPages';
+	import type { AnimalRecord } from '$lib/db/AnimalRecord';
 
 	export let data: AnimalPage;
 	export let error: PostgrestError | null = null;
