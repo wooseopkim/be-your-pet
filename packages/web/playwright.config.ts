b9d815f6082636fc04ca11ff8843587916dbdb12
Wooseop Kim <wooseop@protonmail.com>
2025-02-22 18:07:18 +0900

    Add which browsers/devices to use

--- packages/web/playwright.config.ts
+++ packages/web/playwright.config.ts
@@ -1,4 +1,4 @@
-import type { PlaywrightTestConfig } from "@playwright/test";
+import { devices, type PlaywrightTestConfig } from "@playwright/test";
 
 const config: PlaywrightTestConfig = {
   webServer: {
@@ -7,6 +7,36 @@ const config: PlaywrightTestConfig = {
   },
   testDir: "e2e",
   testMatch: /(.+\.)?(test|spec)\.[jt]s/,
+  projects: [
+    {
+      // https://playwright.dev/docs/browsers#chromium-new-headless-mode
+      use: { ...devices["Desktop Chrome"], channel: "chromium" },
+    },
+    {
+      use: devices["Desktop Edge"],
+    },
+    {
+      use: devices["Desktop Firefox"],
+    },
+    {
+      use: devices["Desktop Safari"],
+    },
+    {
+      use: devices["Pixel 7 landscape"],
+    },
+    {
+      use: devices["Galaxy S III"],
+    },
+    {
+      use: devices["iPad Mini"],
+    },
+    {
+      use: devices["iPhone 15 Pro Max"],
+    },
+    {
+      use: devices["iPhone SE"],
+    },
+  ],
 };
 
 export default config;
