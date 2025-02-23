6f107ae15a43aa55b98d354b67ab4e12a9c42ef5
Wooseop Kim <wooseop@protonmail.com>
2025-02-23 19:12:31 +0900

    Configure Nix formatter

--- shell.nix
+++ shell.nix
@@ -18,18 +18,19 @@ mkShell {
   PLAYWRIGHT_BROWSERS_PATH = playwright-driver.browsers;
   PLAYWRIGHT_HOST_PLATFORM_OVERRIDE = "nixos";
 
-  shellHook = let
-    playwrightPackage = x: "${x}@file:${playwright-test}/lib/node_modules/${x}";
-    playwrightPackages = builtins.map playwrightPackage [
-      "playwright"
-      "playwright-core"
-      "@playwright/test"
-    ];
-  in
-  ''
-    bun install --dev --no-save --exact \
-      ${lib.concatStringsSep " " playwrightPackages}
-    bun --version >.bun-version
-    node --version >.node-version
-  '';
+  shellHook =
+    let
+      playwrightPackage = x: "${x}@file:${playwright-test}/lib/node_modules/${x}";
+      playwrightPackages = builtins.map playwrightPackage [
+        "playwright"
+        "playwright-core"
+        "@playwright/test"
+      ];
+    in
+    ''
+      bun install --dev --no-save --exact \
+        ${lib.concatStringsSep " " playwrightPackages}
+      bun --version >.bun-version
+      node --version >.node-version
+    '';
 }
