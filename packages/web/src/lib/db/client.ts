9ba706cc6844d5471c1b4d24b88abe5d6d2cf971
Wooseop Kim <wooseop@protonmail.com>
2025-02-23 19:31:18 +0900

    Throw when required environment variable is empty

--- packages/web/src/lib/db/client.ts
+++ packages/web/src/lib/db/client.ts
@@ -1,9 +1,19 @@
 import { env } from "$env/dynamic/public";
 import { createClient } from "@supabase/supabase-js";
 
+function readOrThrow(key: keyof typeof env) {
+  const value = env[key];
+  if (!value) {
+    throw new Error(
+      `env[${key}] is ${String(key)} => ${JSON.stringify({ env })}`,
+    );
+  }
+  return value;
+}
+
 const supabase = createClient(
-  env.PUBLIC_SUPABASE_URL ?? "",
-  env.PUBLIC_SUPABASE_ANON_KEY ?? "",
+  readOrThrow("PUBLIC_SUPABASE_URL"),
+  readOrThrow("PUBLIC_SUPABASE_ANON_KEY"),
 );
 
 export default supabase;
