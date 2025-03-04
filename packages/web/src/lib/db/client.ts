77304f24dac9083726fc914cc67eb243d2e166ac
Wooseop Kim <wooseop@protonmail.com>
2025-03-04 23:00:12 +0900

    Use static adapter

--- packages/web/src/lib/db/client.ts
+++ packages/web/src/lib/db/client.ts
@@ -1,19 +1,9 @@
-import { env } from "$env/dynamic/public";
+import {
+  PUBLIC_SUPABASE_ANON_KEY,
+  PUBLIC_SUPABASE_URL,
+} from "$env/static/public";
 import { createClient } from "@supabase/supabase-js";
 
-function readOrThrow(key: keyof typeof env) {
-  const value = env[key];
-  if (!value) {
-    throw new Error(
-      `env[${key}] is ${String(key)} => ${JSON.stringify({ env })}`,
-    );
-  }
-  return value;
-}
-
-const supabase = createClient(
-  readOrThrow("PUBLIC_SUPABASE_URL"),
-  readOrThrow("PUBLIC_SUPABASE_ANON_KEY"),
-);
+const supabase = createClient(PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY);
 
 export default supabase;
--- packages/web/src/routes/+page.server.ts
+++ packages/web/src/routes/+page.server.ts
@@ -3,6 +3,8 @@ import { loadAnimalPage } from "../lib/db/animals";
 
 const MINUTE_IN_MILLIS = 1000 * 60;
 
+export const prerender = true;
+
 export async function load() {
   const size = 5;
   const initialData = await loadAnimalPage({ size });
