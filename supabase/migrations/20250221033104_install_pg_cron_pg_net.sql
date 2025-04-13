0d49a55aca6dbbc915fe5b606cc24b5b7af6cd88
Wooseop Kim <wooseop@protonmail.com>
2025-04-13 21:51:10 +0900

    Change which schema to install extensions in

--- supabase/migrations/20250221033104_install_pg_cron_pg_net.sql
+++ supabase/migrations/20250221033104_install_pg_cron_pg_net.sql
@@ -1,6 +1,6 @@
-create extension pg_cron with schema pg_catalog;
+create extension pg_cron with schema extensions;
 
 grant usage on schema cron to postgres;
 grant all privileges on all tables in schema cron to postgres;
 
-create extension pg_net;
+create extension pg_net with schema extensions;
