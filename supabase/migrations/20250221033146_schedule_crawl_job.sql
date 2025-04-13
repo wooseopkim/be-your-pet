d2a309de780215ed76f4b64ba6a09e52d8078b71
Wooseop Kim <wooseop@protonmail.com>
2025-04-14 08:45:57 +0900

    Deduplicate concat operator

--- supabase/migrations/20250221033146_schedule_crawl_job.sql
+++ supabase/migrations/20250221033146_schedule_crawl_job.sql
@@ -8,7 +8,7 @@ select
           url:='https://vrpyodyxhlbljkedyeoy.supabase.co/functions/v1/crawl?open_api_service_key=' || (select decrypted_secret from vault.decrypted_secrets where name = 'open_api_service_key'),
           headers:=jsonb_build_object(
             'Content-Type', 'application/json',
-            'Authorization', 'Bearer ' || || (select decrypted_secret from vault.decrypted_secrets where name = 'supabase_cron_api_key')
+            'Authorization', 'Bearer ' || (select decrypted_secret from vault.decrypted_secrets where name = 'supabase_cron_api_key')
           ),
           body:=jsonb_build_object('time', now()),
           timeout_milliseconds:=30000
